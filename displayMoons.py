180414bf5b297367b6f8fb43c98a70a67df751ec
Daniel Perez-Becker <dperezbecker@yammmer-inc.com>
2016-12-15 02:17:58 -0600

    added moonphases

--- displayMoons.py
+++ displayMoons.py
@@ -11,30 +11,35 @@ display = SevenSegment.SevenSegment(address=0x71)
 # Initialize the display. Must be called once before using the display.
 display.begin()
 
-# Keep track of the colon being turned on or off.
-colon = False
+
+def fullDisplay(CurrentMoon):
+
+    mm.displayMatrix(CurrentMoon)
+
+    if(CurrentMoon > 100.):
+        CurrentMoon = float("%.1f" % CurrentMoon)
+    else:
+        CurrentMoon = float("%.2f" % CurrentMoon)
+
+    # Clear the display buffer.
+    display.clear()
+    # Print a floating point number to the display.
+    if(CurrentMoon < 100.):
+        display.print_float(CurrentMoon)
+    else:
+        display.print_float(CurrentMoon, decimal_digits=1)
+    # Set the colon on or off (True/False).
+    colon = False
+    display.set_colon(colon)
+    # Write the display buffer to the hardware.  This must be called to
+    # update the actual display LEDs.
+    display.write_display()
+
+    return
 
 CurrentMoon=gm.getCurrentMoon()
+twoDecCurrentMoon = float("%.2f" % CurrentMoon)
 
-mm.displayMoonCycles(CurrentMoon)
-mm.displayMatrix(CurrentMoon)
-
-if(CurrentMoon > 100.):
-    CurrentMoon = float("%.1f" % CurrentMoon)
-else:
-    CurrentMoon = float("%.2f" % CurrentMoon)
-
-# Clear the display buffer.
-display.clear()
-# Print a floating point number to the display.
-if(CurrentMoon < 100.):
-    display.print_float(CurrentMoon)
-else:
-    display.print_float(CurrentMoon, decimal_digits=1)
-# Set the colon on or off (True/False).
-display.set_colon(colon)
-# Write the display buffer to the hardware.  This must be called to
-# update the actual display LEDs.
-display.write_display()
-# Delay for a second.
-time.sleep(1.0)
+for i in int(twoDecCurrentMoon*100.):
+    fullDisplay(twoDecCurrentMoon)
+    time.sleep(0.05)
--- matrixMoons.py
+++ matrixMoons.py
@@ -224,216 +224,216 @@ def displayMatrix(CurrentMoon):
 
     return
 
-def displayMoonCycles(CurrentMoon):
-
-    MMoon=[]
-
-    MMoon.append([[0,0,1,1,1,1,0,0],\
-                  [0,1,1,1,1,1,1,0],\
-                  [1,1,1,1,1,1,1,1],\
-                  [1,1,1,1,1,1,1,1],\
-                  [1,1,1,1,1,1,1,1],\
-                  [1,1,1,1,1,1,1,1],\
-                  [0,1,1,1,1,1,1,0],\
-                  [0,0,1,1,1,1,0,0]])
-
-    MMoon.append([[0,0,1,1,1,1,0,0],\
-            [0,1,1,1,1,1,1,0],\
-            [1,1,1,1,1,1,1,0],\
-            [1,1,1,1,1,1,1,0],\
-            [1,1,1,1,1,1,1,0],\
-            [1,1,1,1,1,1,1,0],\
-            [0,1,1,1,1,1,1,0],\
-            [0,0,1,1,1,1,0,0]])
-
-    MMoon.append([[0,0,1,1,1,0,0,0],\
-            [0,1,1,1,1,1,0,0],\
-            [1,1,1,1,1,1,0,0],\
-            [1,1,1,1,1,1,0,0],\
-            [1,1,1,1,1,1,0,0],\
-            [1,1,1,1,1,1,0,0],\
-            [0,1,1,1,1,1,0,0],\
-            [0,0,1,1,1,0,0,0]])
-
-    MMoon.append([[0,0,1,1,0,0,0,0],\
-            [0,1,1,1,1,0,0,0],\
-            [1,1,1,1,1,0,0,0],\
-            [1,1,1,1,1,0,0,0],\
-            [1,1,1,1,1,0,0,0],\
-            [1,1,1,1,1,0,0,0],\
-            [0,1,1,1,1,0,0,0],\
-            [0,0,1,1,0,0,0,0]])
-
-    MMoon.append([[0,0,1,1,0,0,0,0],\
-            [0,1,1,1,0,0,0,0],\
-            [1,1,1,1,0,0,0,0],\
-            [1,1,1,1,0,0,0,0],\
-            [1,1,1,1,0,0,0,0],\
-            [1,1,1,1,0,0,0,0],\
-            [0,1,1,1,0,0,0,0],\
-            [0,0,1,1,0,0,0,0]])
-
-    MMoon.append([[0,0,1,1,0,0,0,0],\
-            [0,1,1,0,0,0,0,0],\
-            [1,1,1,0,0,0,0,0],\
-            [1,1,1,0,0,0,0,0],\
-            [1,1,1,0,0,0,0,0],\
-            [1,1,1,0,0,0,0,0],\
-            [0,1,1,0,0,0,0,0],\
-            [0,0,1,1,0,0,0,0]])
-
-    MMoon.append([[0,0,1,0,0,0,0,0],\
-            [0,1,0,0,0,0,0,0],\
-            [1,1,0,0,0,0,0,0],\
-            [1,1,0,0,0,0,0,0],\
-            [1,1,0,0,0,0,0,0],\
-            [1,1,0,0,0,0,0,0],\
-            [0,1,0,0,0,0,0,0],\
-            [0,0,1,0,0,0,0,0]])
-
-    MMoon.append([[0,0,1,0,0,0,0,0],\
-            [0,1,0,0,0,0,0,0],\
-            [1,0,0,0,0,0,0,0],\
-            [1,0,0,0,0,0,0,0],\
-            [1,0,0,0,0,0,0,0],\
-            [1,0,0,0,0,0,0,0],\
-            [0,1,0,0,0,0,0,0],\
-            [0,0,1,0,0,0,0,0]])
-
-    MMoon.append([[0,0,0,0,0,0,0,0],\
-            [0,0,0,0,0,0,0,0],\
-            [0,0,0,0,0,0,0,0],\
-            [0,0,0,0,0,0,0,0],\
-            [0,0,0,0,0,0,0,0],\
-            [0,0,0,0,0,0,0,0],\
-            [0,0,0,0,0,0,0,0],\
-            [0,0,0,0,0,0,0,0]])
-
-    MMoon.append([[0,0,0,0,0,1,0,0],\
-            [0,0,0,0,0,0,1,0],\
-            [0,0,0,0,0,0,0,1],\
-            [0,0,0,0,0,0,0,1],\
-            [0,0,0,0,0,0,0,1],\
-            [0,0,0,0,0,0,0,1],\
-            [0,0,0,0,0,0,1,0],\
-            [0,0,0,0,0,1,0,0]])
-
-    MMoon.append([[0,0,0,0,0,1,0,0],\
-            [0,0,0,0,0,0,1,0],\
-            [0,0,0,0,0,0,1,1],\
-            [0,0,0,0,0,0,1,1],\
-            [0,0,0,0,0,0,1,1],\
-            [0,0,0,0,0,0,1,1],\
-            [0,0,0,0,0,0,1,0],\
-            [0,0,0,0,0,1,0,0]])
-
-    MMoon.append([[0,0,0,0,1,1,0,0],\
-            [0,0,0,0,0,1,1,0],\
-            [0,0,0,0,0,1,1,1],\
-            [0,0,0,0,0,1,1,1],\
-            [0,0,0,0,0,1,1,1],\
-            [0,0,0,0,0,1,1,1],\
-            [0,0,0,0,0,1,1,0],\
-            [0,0,0,0,1,1,0,0]])
-
-    MMoon.append([[0,0,0,0,1,1,0,0],\
-            [0,0,0,0,1,1,1,0],\
-            [0,0,0,0,1,1,1,1],\
-            [0,0,0,0,1,1,1,1],\
-            [0,0,0,0,1,1,1,1],\
-            [0,0,0,0,1,1,1,1],\
-            [0,0,0,0,1,1,1,0],\
-            [0,0,0,0,1,1,0,0]])
-
-    MMoon.append([[0,0,0,0,1,1,0,0],\
-            [0,0,0,1,1,1,1,0],\
-            [0,0,0,1,1,1,1,1],\
-            [0,0,0,1,1,1,1,1],\
-            [0,0,0,1,1,1,1,1],\
-            [0,0,0,1,1,1,1,1],\
-            [0,0,0,1,1,1,1,0],\
-            [0,0,0,0,1,1,0,0]])
-
-    MMoon.append([[0,0,0,1,1,1,0,0],\
-            [0,0,1,1,1,1,1,0],\
-            [0,0,1,1,1,1,1,1],\
-            [0,0,1,1,1,1,1,1],\
-            [0,0,1,1,1,1,1,1],\
-            [0,0,1,1,1,1,1,1],\
-            [0,0,1,1,1,1,1,0],\
-            [0,0,0,1,1,1,0,0]])
-
-    MMoon.append([[0,0,1,1,1,1,0,0],\
-            [0,1,1,1,1,1,1,0],\
-            [0,1,1,1,1,1,1,1],\
-            [0,1,1,1,1,1,1,1],\
-            [0,1,1,1,1,1,1,1],\
-            [0,1,1,1,1,1,1,1],\
-            [0,1,1,1,1,1,1,0],\
-            [0,0,1,1,1,1,0,0]])
-
-    CurrentMoonFraction=CurrentMoon-int(CurrentMoon)
-
-    # if(CurrentMoonFraction >= 0.96875 or CurrentMoonFraction < 0.03125):
-    #     MMoon=Moon00
-    # elif(CurrentMoonFraction >= 0.03125 and CurrentMoonFraction < 0.09375):
-    #     MMoon=Moon01
-    # elif(CurrentMoonFraction >= 0.09375 and CurrentMoonFraction < 0.15625):
-    #     MMoon=Moon02
-    # elif(CurrentMoonFraction >= 0.15625 and CurrentMoonFraction < 0.21875):
-    #     MMoon=Moon03
-    # elif(CurrentMoonFraction >= 0.21875 and CurrentMoonFraction < 0.28125):
-    #     MMoon=Moon04
-    # elif(CurrentMoonFraction >= 0.28125 and CurrentMoonFraction < 0.34375):
-    #     MMoon=Moon05
-    # elif(CurrentMoonFraction >= 0.34375 and CurrentMoonFraction < 0.40625):
-    #     MMoon=Moon06
-    # elif(CurrentMoonFraction >= 0.40625 and CurrentMoonFraction < 0.46875):
-    #     MMoon=Moon07
-    # elif(CurrentMoonFraction >= 0.46875 and CurrentMoonFraction < 0.53125):
-    #     MMoon=Moon08
-    # elif(CurrentMoonFraction >= 0.53125 and CurrentMoonFraction < 0.59375):
-    #     MMoon=Moon09
-    # elif(CurrentMoonFraction >= 0.59375 and CurrentMoonFraction < 0.65625):
-    #     MMoon=Moon10
-    # elif(CurrentMoonFraction >= 0.65625 and CurrentMoonFraction < 0.71875):
-    #     MMoon=Moon11
-    # elif(CurrentMoonFraction >= 0.71875 and CurrentMoonFraction < 0.78125):
-    #     MMoon=Moon12
-    # elif(CurrentMoonFraction >= 0.78125 and CurrentMoonFraction < 0.84375):
-    #     MMoon=Moon13
-    # elif(CurrentMoonFraction >= 0.84375 and CurrentMoonFraction < 0.90625):
-    #     MMoon=Moon14
-    # elif(CurrentMoonFraction >= 0.90625 and CurrentMoonFraction < 0.96875):
-    #     MMoon=Moon15
-    # else:
-    #     MMoon=MoonErr
-
-
-
-    # Create display instance on default I2C address (0x70) and bus number.
-    display = Matrix8x8.Matrix8x8()
-
-    # Alternatively, create a display with a specific I2C address and/or bus.
-    # display = Matrix8x8.Matrix8x8(address=0x74, busnum=1)
-
-    # Initialize the display. Must be called once before using the display.
-    display.begin()
-
-    # Set display brightness (15 is max, 0 is min).
-    display.set_brightness(5)
-
-    for i in range(int(CurrentMoon)):
-        for j in range(len(MMoon)):
-            display.clear()
-            for x in range(8):
-            	for y in range(8):
-            		# Set pixel at position i, j to on.  To turn off a pixel set
-            		# the last parameter to 0.
-            		display.set_pixel(x, y, MMoon[j][y][x])
-            		# Write the display buffer to the hardware.  This must be called to
-            		# update the actual display LEDs.
-            display.write_display()
-            time.sleep(0.05)
-
-    return
+# def displayMoonCycles(CurrentMoon):
+#
+#     MMoon=[]
+#
+#     MMoon.append([[0,0,1,1,1,1,0,0],\
+#                   [0,1,1,1,1,1,1,0],\
+#                   [1,1,1,1,1,1,1,1],\
+#                   [1,1,1,1,1,1,1,1],\
+#                   [1,1,1,1,1,1,1,1],\
+#                   [1,1,1,1,1,1,1,1],\
+#                   [0,1,1,1,1,1,1,0],\
+#                   [0,0,1,1,1,1,0,0]])
+#
+#     MMoon.append([[0,0,1,1,1,1,0,0],\
+#             [0,1,1,1,1,1,1,0],\
+#             [1,1,1,1,1,1,1,0],\
+#             [1,1,1,1,1,1,1,0],\
+#             [1,1,1,1,1,1,1,0],\
+#             [1,1,1,1,1,1,1,0],\
+#             [0,1,1,1,1,1,1,0],\
+#             [0,0,1,1,1,1,0,0]])
+#
+#     MMoon.append([[0,0,1,1,1,0,0,0],\
+#             [0,1,1,1,1,1,0,0],\
+#             [1,1,1,1,1,1,0,0],\
+#             [1,1,1,1,1,1,0,0],\
+#             [1,1,1,1,1,1,0,0],\
+#             [1,1,1,1,1,1,0,0],\
+#             [0,1,1,1,1,1,0,0],\
+#             [0,0,1,1,1,0,0,0]])
+#
+#     MMoon.append([[0,0,1,1,0,0,0,0],\
+#             [0,1,1,1,1,0,0,0],\
+#             [1,1,1,1,1,0,0,0],\
+#             [1,1,1,1,1,0,0,0],\
+#             [1,1,1,1,1,0,0,0],\
+#             [1,1,1,1,1,0,0,0],\
+#             [0,1,1,1,1,0,0,0],\
+#             [0,0,1,1,0,0,0,0]])
+#
+#     MMoon.append([[0,0,1,1,0,0,0,0],\
+#             [0,1,1,1,0,0,0,0],\
+#             [1,1,1,1,0,0,0,0],\
+#             [1,1,1,1,0,0,0,0],\
+#             [1,1,1,1,0,0,0,0],\
+#             [1,1,1,1,0,0,0,0],\
+#             [0,1,1,1,0,0,0,0],\
+#             [0,0,1,1,0,0,0,0]])
+#
+#     MMoon.append([[0,0,1,1,0,0,0,0],\
+#             [0,1,1,0,0,0,0,0],\
+#             [1,1,1,0,0,0,0,0],\
+#             [1,1,1,0,0,0,0,0],\
+#             [1,1,1,0,0,0,0,0],\
+#             [1,1,1,0,0,0,0,0],\
+#             [0,1,1,0,0,0,0,0],\
+#             [0,0,1,1,0,0,0,0]])
+#
+#     MMoon.append([[0,0,1,0,0,0,0,0],\
+#             [0,1,0,0,0,0,0,0],\
+#             [1,1,0,0,0,0,0,0],\
+#             [1,1,0,0,0,0,0,0],\
+#             [1,1,0,0,0,0,0,0],\
+#             [1,1,0,0,0,0,0,0],\
+#             [0,1,0,0,0,0,0,0],\
+#             [0,0,1,0,0,0,0,0]])
+#
+#     MMoon.append([[0,0,1,0,0,0,0,0],\
+#             [0,1,0,0,0,0,0,0],\
+#             [1,0,0,0,0,0,0,0],\
+#             [1,0,0,0,0,0,0,0],\
+#             [1,0,0,0,0,0,0,0],\
+#             [1,0,0,0,0,0,0,0],\
+#             [0,1,0,0,0,0,0,0],\
+#             [0,0,1,0,0,0,0,0]])
+#
+#     MMoon.append([[0,0,0,0,0,0,0,0],\
+#             [0,0,0,0,0,0,0,0],\
+#             [0,0,0,0,0,0,0,0],\
+#             [0,0,0,0,0,0,0,0],\
+#             [0,0,0,0,0,0,0,0],\
+#             [0,0,0,0,0,0,0,0],\
+#             [0,0,0,0,0,0,0,0],\
+#             [0,0,0,0,0,0,0,0]])
+#
+#     MMoon.append([[0,0,0,0,0,1,0,0],\
+#             [0,0,0,0,0,0,1,0],\
+#             [0,0,0,0,0,0,0,1],\
+#             [0,0,0,0,0,0,0,1],\
+#             [0,0,0,0,0,0,0,1],\
+#             [0,0,0,0,0,0,0,1],\
+#             [0,0,0,0,0,0,1,0],\
+#             [0,0,0,0,0,1,0,0]])
+#
+#     MMoon.append([[0,0,0,0,0,1,0,0],\
+#             [0,0,0,0,0,0,1,0],\
+#             [0,0,0,0,0,0,1,1],\
+#             [0,0,0,0,0,0,1,1],\
+#             [0,0,0,0,0,0,1,1],\
+#             [0,0,0,0,0,0,1,1],\
+#             [0,0,0,0,0,0,1,0],\
+#             [0,0,0,0,0,1,0,0]])
+#
+#     MMoon.append([[0,0,0,0,1,1,0,0],\
+#             [0,0,0,0,0,1,1,0],\
+#             [0,0,0,0,0,1,1,1],\
+#             [0,0,0,0,0,1,1,1],\
+#             [0,0,0,0,0,1,1,1],\
+#             [0,0,0,0,0,1,1,1],\
+#             [0,0,0,0,0,1,1,0],\
+#             [0,0,0,0,1,1,0,0]])
+#
+#     MMoon.append([[0,0,0,0,1,1,0,0],\
+#             [0,0,0,0,1,1,1,0],\
+#             [0,0,0,0,1,1,1,1],\
+#             [0,0,0,0,1,1,1,1],\
+#             [0,0,0,0,1,1,1,1],\
+#             [0,0,0,0,1,1,1,1],\
+#             [0,0,0,0,1,1,1,0],\
+#             [0,0,0,0,1,1,0,0]])
+#
+#     MMoon.append([[0,0,0,0,1,1,0,0],\
+#             [0,0,0,1,1,1,1,0],\
+#             [0,0,0,1,1,1,1,1],\
+#             [0,0,0,1,1,1,1,1],\
+#             [0,0,0,1,1,1,1,1],\
+#             [0,0,0,1,1,1,1,1],\
+#             [0,0,0,1,1,1,1,0],\
+#             [0,0,0,0,1,1,0,0]])
+#
+#     MMoon.append([[0,0,0,1,1,1,0,0],\
+#             [0,0,1,1,1,1,1,0],\
+#             [0,0,1,1,1,1,1,1],\
+#             [0,0,1,1,1,1,1,1],\
+#             [0,0,1,1,1,1,1,1],\
+#             [0,0,1,1,1,1,1,1],\
+#             [0,0,1,1,1,1,1,0],\
+#             [0,0,0,1,1,1,0,0]])
+#
+#     MMoon.append([[0,0,1,1,1,1,0,0],\
+#             [0,1,1,1,1,1,1,0],\
+#             [0,1,1,1,1,1,1,1],\
+#             [0,1,1,1,1,1,1,1],\
+#             [0,1,1,1,1,1,1,1],\
+#             [0,1,1,1,1,1,1,1],\
+#             [0,1,1,1,1,1,1,0],\
+#             [0,0,1,1,1,1,0,0]])
+#
+#     CurrentMoonFraction=CurrentMoon-int(CurrentMoon)
+#
+#     # if(CurrentMoonFraction >= 0.96875 or CurrentMoonFraction < 0.03125):
+#     #     MMoon=Moon00
+#     # elif(CurrentMoonFraction >= 0.03125 and CurrentMoonFraction < 0.09375):
+#     #     MMoon=Moon01
+#     # elif(CurrentMoonFraction >= 0.09375 and CurrentMoonFraction < 0.15625):
+#     #     MMoon=Moon02
+#     # elif(CurrentMoonFraction >= 0.15625 and CurrentMoonFraction < 0.21875):
+#     #     MMoon=Moon03
+#     # elif(CurrentMoonFraction >= 0.21875 and CurrentMoonFraction < 0.28125):
+#     #     MMoon=Moon04
+#     # elif(CurrentMoonFraction >= 0.28125 and CurrentMoonFraction < 0.34375):
+#     #     MMoon=Moon05
+#     # elif(CurrentMoonFraction >= 0.34375 and CurrentMoonFraction < 0.40625):
+#     #     MMoon=Moon06
+#     # elif(CurrentMoonFraction >= 0.40625 and CurrentMoonFraction < 0.46875):
+#     #     MMoon=Moon07
+#     # elif(CurrentMoonFraction >= 0.46875 and CurrentMoonFraction < 0.53125):
+#     #     MMoon=Moon08
+#     # elif(CurrentMoonFraction >= 0.53125 and CurrentMoonFraction < 0.59375):
+#     #     MMoon=Moon09
+#     # elif(CurrentMoonFraction >= 0.59375 and CurrentMoonFraction < 0.65625):
+#     #     MMoon=Moon10
+#     # elif(CurrentMoonFraction >= 0.65625 and CurrentMoonFraction < 0.71875):
+#     #     MMoon=Moon11
+#     # elif(CurrentMoonFraction >= 0.71875 and CurrentMoonFraction < 0.78125):
+#     #     MMoon=Moon12
+#     # elif(CurrentMoonFraction >= 0.78125 and CurrentMoonFraction < 0.84375):
+#     #     MMoon=Moon13
+#     # elif(CurrentMoonFraction >= 0.84375 and CurrentMoonFraction < 0.90625):
+#     #     MMoon=Moon14
+#     # elif(CurrentMoonFraction >= 0.90625 and CurrentMoonFraction < 0.96875):
+#     #     MMoon=Moon15
+#     # else:
+#     #     MMoon=MoonErr
+#
+#
+#
+#     # Create display instance on default I2C address (0x70) and bus number.
+#     display = Matrix8x8.Matrix8x8()
+#
+#     # Alternatively, create a display with a specific I2C address and/or bus.
+#     # display = Matrix8x8.Matrix8x8(address=0x74, busnum=1)
+#
+#     # Initialize the display. Must be called once before using the display.
+#     display.begin()
+# 
+#     # Set display brightness (15 is max, 0 is min).
+#     display.set_brightness(5)
+#
+#     for i in range(int(CurrentMoon)):
+#         for j in range(len(MMoon)):
+#             display.clear()
+#             for x in range(8):
+#             	for y in range(8):
+#             		# Set pixel at position i, j to on.  To turn off a pixel set
+#             		# the last parameter to 0.
+#             		display.set_pixel(x, y, MMoon[j][y][x])
+#             		# Write the display buffer to the hardware.  This must be called to
+#             		# update the actual display LEDs.
+#             display.write_display()
+#             time.sleep(0.05)
+#
+#     return
